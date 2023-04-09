d588de6dea5a99b6b227b4d2b80d3b520c85004b
athosmfr <athos.mfrodrigues@gmail.com>
2023-04-09 20:27:22 -0300

    init2

--- index.js
+++ index.js
@@ -1,29 +1,68 @@
 const fs = require('fs');
+// const mysql = require('mysql2');
 
-const arquivo = fs.readFileSync('C:/Users/athos/OneDrive/Documentos/arquivo.txt', 'utf-8');
-const linhas = arquivo.split('\n');
+// configuração da conexão com o banco de dados MySQL
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'root',
+//     database: 'api_caixa'
+//   });
 
-// const data = linhas[0].slice(143, 151);
-const dia = linhas[0].slice(143, 145);
-const mes = linhas[0].slice(145, 147);
-const ano = linhas[0].slice(147, 151);
-// const valor = linhas[4].slice(18, 28);
-// const preco = linhas[4].slice(18,26);
-// const centavos = linhas[4].slice(26, 28);
-const preco = linhas[2].slice(86, 98);
-const centavos = linhas[2].slice(98, 100);
+  const fs = require('fs');
+  const path = require('path');
+  
+  const directoryPath = path.join(__dirname, '../hotFolder');
+  
+  fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+          return console.log(`Erro ao ler diretório: ${err}`);
+      }
+  
+      const filteredFiles = files.filter((file) =>
+          fs.statSync(path.join(directoryPath, file)).isFile()
+      );
+  
+      filteredFiles.forEach((file) => {
+          // Adquirindo arquivo e linhas das remessas
+          const filePath = path.join(directoryPath, file);
+          const arquivo = fs.readFileSync(filePath, 'utf-8');
+          const linhas = arquivo.split('\n');
+          // "Extraindo" valores das remessas
+          const dia = linhas[0].slice(143, 145);
+          const mes = linhas[0].slice(145, 147);
+          const ano = linhas[0].slice(147, 151);
+          const preco = linhas[2].slice(86, 98);
+          const centavos = linhas[2].slice(98, 100);
+          const diaVencimento = linhas[2].slice(77, 79);
+          const mesVencimento = linhas[2].slice(79, 81);
+          const anoVencimento = linhas[2].slice(81, 85);
+          const nomePagador = linhas[3].slice(33, 73);
+          const nomeBeneficiario = linhas[0].slice(72, 102);
+          const valorTitulo = Number(`${preco}.${centavos}`);
+  
+          const dadosRemessa = {
+              remessa: file,
+              dataBoleto: `${dia}/${mes}/${ano}`,
+              nomeBeneficiario: nomeBeneficiario.trimEnd(),
+              nomePagador: nomePagador.trimEnd(),
+              dataGeracao: `${dia}/${mes}/${ano}`.trimEnd(),
+              dataVencimento: `${diaVencimento}/${mesVencimento}/${anoVencimento}`.trimEnd(),
+              valor: `R$ ${valorTitulo}`
+          };
+  
+          console.log(dadosRemessa);
+      });
+  });
 
-const diaVencimento = linhas[2].slice(77, 79);
-const mesVencimento = linhas[2].slice(79, 81);
-const anoVencimento = linhas[2].slice(81, 85);
+// connection.connect();
 
-const nomePagador = linhas[3].slice(33, 73)
+// const sql = `INSERT INTO remessas (nome, data_geracao, data_vencimento, valor) 
+// VALUES ('${nomePagador}', '${ano}-${mes}-${dia}', '${anoVencimento}-${mesVencimento}-${diaVencimento}', '${preco}.${centavos}')`;
 
-// console.log(`Data: ${data}`);
-// console.log(`Valores: ${valor}`);
-console.log(`${dia}/${mes}/${ano}`);
-console.log(`R$ ${preco},${centavos}`)
-
-const dados = `Nome do Pagador: ${nomePagador}\nData de Geração: ${dia}/${mes}/${ano}\nData de Vencimento: ${diaVencimento}/${mesVencimento}/${anoVencimento}\nValor: R$ ${preco},${centavos}`;
-
-fs.writeFileSync('C:/Users/athos/OneDrive/Documentos/saida.txt', dados);
+// connection.query(sql, function (error, results, fields) {
+//     if (error) throw error;
+//     console.log('Dados inseridos com sucesso!');
+//   });
+  
+//   connection.end();
